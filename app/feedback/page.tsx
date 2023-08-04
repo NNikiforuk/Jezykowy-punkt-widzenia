4ce961d199e84ce6a620b326b19140a5410357fa
Natalia <natnikiforuk@gmail.com>
2023-08-04 19:29:02 +0200

    Feedback and its RWD

---
+++ app/feedback/page.tsx
@@ -0,0 +1,21 @@
+import { feedback } from "../../data/feedback";
+
+const Feedback = () => {
+	return (
+		<div className="feedback">
+			<h1>Opinie naszych kursantów</h1>
+			<div className="container">
+				{feedback.map((feed) => (
+					<div className="feed" key={feed.id}>
+						<div className="desc">
+							{feed.desc}
+							<span className="name"> {feed.name}</span>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default Feedback;
---
+++ data/feedback.js
@@ -0,0 +1,22 @@
+export const feedback = [
+	{
+		id: 1,
+		name: "Jola",
+		desc: "Kasia to zafascynowana językiem i potrafiąca przekazać tę fascynację dalej. Z pasją dokłada wszelkich starań, by przytoczyć kwestie i rozwiać wątpliwości. Gdy brakuje mi jakiegoś słowa, umie temu zaradzić, taktownie zachęcają do pokonywania trudności. Jej lekcje zawsze wnoszą coś nowego. Cieszę się, że na moeje drodze poznawania języka niemieckiego miałam szczęście spotkać tak wyjątkową nauczycielkę.",
+	},
+	{
+		id: 2,
+		name: "Milena",
+		desc: "Ehrliche Meinung: Kasia = sehr freundlich, geduldige und glückliche Person. Es ist ein Vergnügung, die Nachhilfe mit ihr zu haben. Sie ist der perfekte Lehrerin, weil sie nach dem Unterricht die Zeit nimmt meine Hausaufgaben zu checken. Sie ist immer im Unterricht vorbereitet. Sie korrigiert Fehler während des Gesprächs, was mir am wichtigsten ist. Ich empfehle Kasia von ganzem Herzen ❤",
+	},
+	{
+		id: 3,
+		name: "Michał",
+		desc: "Miałem już kilku korepetytorów, takich którzy mnie nie słuchali, robili coś innego, również byli tacy u których lekcja zaczynała się po 20 minutach. U Kasi nic takiego się nie dzieje, jest w pełni profesjonalna w tym co robi, zawsze skupiona na tym o czym rozmawiamy, w pełni zaangażowana i zmotywowana (czasem bardziej niż ja). Kasia jest serdeczną i miłą osobą obdarzoną dużą dozą cierpliwości. Poświęca mnóstwo czasu poza zajęciami na sprawdzanie zadań domowych, z czym spotkałem się poraz pierwszy. A jej zajęcia przede wszystkim nie są nudne, zawsze w jakiś sposób urozmaicone i inne. Z czystym sumienieniem polecam Kasię 🙂",
+	},
+	{
+		id: 4,
+		name: "Natalia, Jakub",
+		desc: "Zajęcia z Kasią były świetne przede wszystkim ze względu na możliwość uczestniczenia w parze. Godzina mijała bardzo szybko, wszelkie ćwiczenia były bardzo interesujące i angażujące. To sprawiło, że nauka języka niemieckiego stała się przyjemnością, a nie przykrą koniecznością.",
+	},
+];
