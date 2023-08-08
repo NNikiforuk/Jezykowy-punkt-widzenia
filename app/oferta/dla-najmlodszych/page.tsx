caa4133c3e6747feb71a38bc2945baa4d7a4192d
Natalia <natnikiforuk@gmail.com>
2023-08-08 16:51:56 +0200

    Fixing styling for images on youngest offer page

--- app/oferta/dla-najmlodszych/page.tsx
+++ app/oferta/dla-najmlodszych/page.tsx
@@ -7,7 +7,7 @@ const Youngest = () => {
 	return (
 		<div className="youngest">
 			<h1>Oferta dla najmłodszych</h1>
-			<div className="desc">
+			<div className="youngest_desc">
 				<div className="desc_element">
 					<Image
 						src="/assets/images/offer-child.png"
@@ -18,28 +18,28 @@ const Youngest = () => {
 						priority={false}
 					/>
 					<p className="text">Zajęcia indywidualne lub grupowe do 4 osób</p>
-					<div className="desc_element">
-						<Image
-							src="/assets/images/offer-kids.png"
-							alt="group of children"
-							width={120}
-							height={80}
-							className="img"
-							priority={false}
-						/>
-						<p className="text">Różne grupy wiekowe</p>
-					</div>
-					<div className="desc_element">
-						<Image
-							src="/assets/images/offer-play.png"
-							alt="children playing"
-							width={100}
-							height={90}
-							className="img"
-							priority={false}
-						/>
-						<p className="text">Nauka poprzez zabawę</p>
-					</div>
+				</div>
+				<div className="desc_element">
+					<Image
+						src="/assets/images/offer-kids.png"
+						alt="group of children"
+						width={120}
+						height={80}
+						className="img"
+						priority={false}
+					/>
+					<p className="text">Różne grupy wiekowe</p>
+				</div>
+				<div className="desc_element">
+					<Image
+						src="/assets/images/offer-play.png"
+						alt="children playing"
+						width={100}
+						height={90}
+						className="img"
+						priority={false}
+					/>
+					<p className="text">Nauka poprzez zabawę</p>
 				</div>
 			</div>
 			<Packages />
