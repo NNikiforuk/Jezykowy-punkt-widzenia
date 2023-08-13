a7ecd635ab8b86e6a255a0096208847e518e470e
Natalia <natnikiforuk@gmail.com>
2023-08-13 14:09:05 +0200

    Minimum navbar for mobile

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -7,15 +7,10 @@ import { useEffect, useRef, useState } from "react";
 import Navbar_links from "./Navbar_links";
 
 const Navbar = () => {
-	const mobileMediaQuery = "(max-width: 1020px)";
-	const navPrimaryRef = useRef();
+	const [showMobileNav, setShowMobileNav] = useState<boolean>(false);
 
-	const [isMobile, setIsMobile] = useState(
-		window.matchMedia(mobileMediaQuery).matches
-	);
-
-	const showNavMobileSecondaryDropdown = () => {
-		navPrimaryRef.current.classList.toggle("toggle_primaryDropdown");
+	const toggleMobileNav = () => {
+		setShowMobileNav((prev) => !prev);
 	};
 
 	return (
@@ -30,49 +25,43 @@ const Navbar = () => {
 					priority={false}
 				/>
 			</Link>
-			{isMobile ? (
-				<>
-					<nav ref={navPrimaryRef} className="nav_mobile">
-						<div className="nav_mobile_link">
-							<div className="dropdown_title-desktop">
-								<div>Oferta</div>
-								<FaAngleRight
-									onClick={showNavMobileSecondaryDropdown}
-									className="right-arrow-icon"
-								/>
-							</div>
-
-							<Navbar_links
-								navMobileDropdown={navMobileDropdown}
-								showNavMobileDropdown={showNavMobileDropdown}
-							/>
-						</div>
-						<Link href="/blog">Blog</Link>
-						<Link href="/about-me">About me</Link>
-						<button
-							onClick={toggleBothMobileNavbars}
-							className="nav-btn nav-close-btn"
-						>
-							<FaTimes />
-						</button>
-					</nav>
-					<button onClick={showNavbar} className="nav-btn">
+			{/* Mobile */}
+			<>
+				<nav>
+					<button className="nav-btn" onClick={toggleMobileNav}>
 						<FaBars />
 					</button>
-				</>
-			) : (
-				<nav className="desktop_nav" ref={navDesktopRef}>
-					<Link href="/">Home</Link>
-					<div className="link">
-						<div className="dropdown_title-desktop" onClick={showDesktopNavbar}>
-							<div>My work</div>
-							<FaAngleDown className="down-arrow-icon" />
-						</div>
-					</div>
-					<Link href="/blog">Blog</Link>
-					<Link href="/about-me">About me</Link>
 				</nav>
-			)}
+				<div className={`mobileNavDropdown ${showMobileNav ? "active" : ""}`}>
+					<Link href="/oferta" onClick={toggleMobileNav}>
+						Oferta
+					</Link>
+					<Link href="/cennik" onClick={toggleMobileNav}>
+						Cennik
+					</Link>
+					<Link href="/tlumaczenia" onClick={toggleMobileNav}>
+						Tłumaczenia
+					</Link>
+					<Link href="/lektorzy" onClick={toggleMobileNav}>
+						Lektorzy
+					</Link>
+					<Link href="/artykuly" onClick={toggleMobileNav}>
+						Artykuły
+					</Link>
+					<Link href="/opinie" onClick={toggleMobileNav}>
+						Opinie
+					</Link>
+					<Link href="/kontakt" onClick={toggleMobileNav}>
+						Kontakt
+					</Link>
+					<Link href="/sklep" onClick={toggleMobileNav}>
+						Sklep
+					</Link>
+					<button className="nav-btn nav-close-btn" onClick={toggleMobileNav}>
+						<FaTimes />
+					</button>
+				</div>
+			</>
 		</header>
 	);
 };
