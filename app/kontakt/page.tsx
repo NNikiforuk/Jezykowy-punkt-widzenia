52a6ba048d68ff8bda3af150811cd025f401a2c0
Natalia <natnikiforuk@gmail.com>
2023-08-16 15:29:48 +0200

    Contact page completed

---
+++ app/kontakt/page.tsx
@@ -0,0 +1,24 @@
+import { FaPhone } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+
+const Contact = () => {
+	return (
+		<div className="contact">
+			<h1>Zapraszamy do kontaktu</h1>
+			<div className="tel">
+				<div className="tel_icon">
+					<FaPhone />
+				</div>
+				<div className="tel_number">(+48) 603 156 142</div>
+			</div>
+			<div className="email">
+				<div className="email_icon">
+					<AiOutlineMail />
+				</div>
+				<div className="email_address">kontakt@jpw-kursy.pl</div>
+			</div>
+		</div>
+	);
+};
+
+export default Contact;
