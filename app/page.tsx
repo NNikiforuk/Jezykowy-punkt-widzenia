493b4a5469cde08b1f5473630ff06357fdfe293c
Natalia <natnikiforuk@gmail.com>
2023-08-21 22:45:32 +0200

    Landing page for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import { CgArrowLongRightR } from "react-icons/cg";
+import { RiArrowLeftSFill, RiArrowRightSFill } from "react-icons/ri";
+
 const Home = () => {
 	return (
 		<div className="home">
-			<div>Home content</div>
+			<h1 className="home_headline">JĘZYKOWY PUNKT WIDZENIA</h1>
+			<div className="arrow_icon">
+				<CgArrowLongRightR />
+			</div>
+			<h2 className="home_headline-secondary">KURSY JĘZYKOWE</h2>
+			<p>ZAJĘCIA ONLINE</p>
+			<div className="time">
+				<div className="left_wing">
+					<RiArrowLeftSFill />
+				</div>
+				<div>OD PON DO PT</div>
+				<div className="right_wing">
+					<RiArrowRightSFill />
+				</div>
+			</div>
 		</div>
 	);
 };
