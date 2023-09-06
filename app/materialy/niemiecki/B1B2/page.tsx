5c3179ab48f059506f613339fea88222708ccfcb
Natalia <natnikiforuk@gmail.com>
2023-09-06 23:18:53 +0200

    B1B2 materials

--- app/materialy/niemiecki/B1B2/page.tsx
+++ app/materialy/niemiecki/B1B2/page.tsx
@@ -1,8 +1,204 @@
+import Image from "next/image";
+
 const GermanB1B2 = () => {
 	return (
-		<div className="germanB1B2">
-			<h1>Materiały z j. niemieckiego</h1>
-			<h2>B1/B2</h2>
+		<div className="german">
+			<h1>Materiały z j. niemieckiego B1/B2</h1>
+			<div className="german_container">
+				<div className="text_container">
+					<div className="title">Korallen</div>
+					<div className="wrapper">
+						<div className="text">
+							<p className="paragraph">
+								Wenn wir uns über Korallen und Korallenriffe
+								<span className="blue_text"> informieren¹</span> – dann waren
+								das bisher meistens schlechte Nachrichten. Auch das hat dem
+								<span className="green_text"> Klimawande²</span> zu tun – der
+								hat nämlich dafür gesorgt, dass das Meerwasser wärmer geworden
+								<span className="green_text"> ist³</span> und den Korallen tut
+								das gar nicht gut.
+							</p>
+							<div className="photos">
+								<div className="photo">
+									<div className="desc">
+										So sollen sie ja eigentlich aussehen:
+									</div>
+									<div className="image">
+										<Image
+											src="/assets/images/coral.png"
+											alt="coral image"
+											width={200}
+											height={130}
+										/>
+									</div>
+								</div>
+								<div className="photo">
+									<div className="desc">
+										Mittlerweile <span className="green_text">sehen</span> die
+										meisten Korallen aber so
+										<span className="green_text"> aus⁴</span>:
+									</div>
+									<div className="image">
+										<Image
+											src="/assets/images/water.png"
+											alt="water image"
+											width={200}
+											height={130}
+										/>
+									</div>
+								</div>
+							</div>
+
+							<p className="paragraph">
+								Sie haben leider ihre Farbe
+								<span className="blue_text"> verloren⁵</span>. Ihr könnt euch
+								also <span className="green_text"> vorstellen⁶</span>, dass die
+								Forscherinnen und Forscher ziemlich überrascht waren
+								<span className="green_text"> als⁷</span> sie dieses neue,
+								gesunde Korallenriff
+								<span className="blue_text"> entdecken⁸</span> haben.
+							</p>
+
+							<p className="paragraph bold">
+								„Wunderschön ist es hier unten. Wie eine riesige Blumenwiese
+								erstreckt sich das Korallenriff über drei Kilometer lang. Die
+								großen Korallen hier nennt man auch „Rosenkorallen“
+							</p>
+
+							<p className="paragraph">
+								- <span className="green_text">weil⁹</span> sie ein bisschen wie
+								sehr große Rosen aussehen. Manche von ihnen sind über zwei Meter
+								breit. Das neuentdeckte Riff ist eines der größten auf der Welt.
+							</p>
+							<p className="paragraph">
+								Es liegt in der Nähe von Tahiti. Die Insel Tahiti lieht zwischen
+								Australien und Südamerika, mitten im Pazifischen Ozean. Das
+								besondere am Korallenriff ist, dass es noch sehr gut erhalten.
+								Viel besser als die meisten anderen Riffe, von
+								<span className="green_text"> denen¹⁰</span> wir Menschen
+								wissen.
+								<span className="blue_text"> Wahrscheinlich¹¹</span> liegt das
+								daran, dass das Korallenriff sehr weit unten liegt – in 35-70
+								Metern Tiefe. Das Wasser so tief unten ist kühler als in
+								flacheren <span className="blue_text">Gegenden¹²</span>. So
+								<span className="green_text"> wird¹³</span> den Korallen nicht
+								zu warm. Jetzt wird das Riff erstmal ganz genau erforscht. Hier
+								ist noch richtig viel zu
+								<span className="green_text"> entdecken¹⁴</span>.
+							</p>
+						</div>
+						<div className="legend">
+							<ul>
+								<li className="blue_text">
+									<sup>1</sup> informieren ≈ vorlegen, erklären
+								</li>
+								<li className="green_text">
+									<sup>2</sup> przysłówek zaimkowy
+								</li>
+								<li className="green_text">
+									<sup>3</sup> czas przeszły perfekt
+								</li>
+								<li className="green_text">
+									<sup>4</sup> czasownik rozdzielnie złożony
+								</li>
+								<li className="blue_text">
+									<sup>5</sup> verlieren ≈ nicht zu finden
+								</li>
+								<li className="green_text">
+									<sup>6</sup> tryb przypuszczający
+								</li>
+								<li className="green_text">
+									<sup>7</sup> spójnik als stosowany do porównań
+								</li>
+
+								<li className="blue_text">
+									<sup>8</sup> entdecken ≈ erfahren, erkennen
+								</li>
+								<li className="green_text">
+									<sup>9</sup> spójnik po którym występuje szyk końcowy
+								</li>
+								<li className="green_text">
+									<sup>10</sup> zdanie przydawkowe
+								</li>
+								<li className="blue_text">
+									<sup>11</sup> Wahrscheinlich ≈ womöglich, vermutlich
+								</li>
+								<li className="blue_text">
+									<sup>12</sup> Gegend ≈ Ort, Gebiet , Fläche
+								</li>
+								<li className="green_text">
+									<sup>13</sup> czas przyszły Futur I
+								</li>
+								<li className="green_text">
+									<sup>14</sup> konstrukcja gramatyczna z „zu”
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div className="text_container">
+					<div className="title">Schulbus in Indien</div>
+					<div className="wrapper">
+						<div className="text">
+							<p className="paragraph italic">
+								<span className="bold">Tipps:</span> Auf hellgrün habe ich die
+								Wörter markiert, die mit der Grammatik verbunden sind und auf
+								hellblau, die die andere Bedeutung haben könnten.
+							</p>
+							<p className="paragraph">
+								In Indien gibt <span className="green_text">es¹ </span>
+								Schulbusse, <span className="green_text">die²</span> durch die
+								Stadt <span className="green_text">fahren³</span> wo die Kinder
+								an unterschiedlichen Orten
+								<span className="blue_text">einsteigen⁴</span>
+								<span className="green_text"> können⁵</span>. An einer Schule
+								halten diese Busse aber nie und trotzdem lernen die Kinder
+								etwas. Mitten in den Slums, in den Armen-vierteln der riesigen
+								<span className="green_text"> Stadt⁶</span> Delhi, ist dieser
+								Bus unterwegs.
+							</p>
+							<p className="paragraph">
+								
+							</p>
+						</div>
+						<div className="legend">
+							<ul>
+								<li className="blue_text">
+									<sup>1</sup> Mein Name ≈ Ich heiße
+								</li>
+								<li className="green_text">
+									<sup>2</sup> gdy mówimy o wieku to stosujemy czasownik sein
+								</li>
+								<li className="green_text">
+									<sup>3</sup> odmiana rzeczownika przez przypadki Akkusativ +
+									rodzajnik nieokreślony
+								</li>
+								<li className="blue_text">
+									<sup>4</sup> wohnen ≈ leben
+								</li>
+								<li className="blue_text">
+									<sup>5</sup> czasownik regularny, ale z reguł
+								</li>
+								<li className="blue_text">
+									<sup>6</sup> arbeiten ≈ beschäftigen
+								</li>
+								<li className="green_text">
+									<sup>7</sup> czasownik modalny
+								</li>
+								<li className="green_text">
+									<sup>8</sup> czasownik rozdzielnie złożony
+								</li>
+								<li className="green_text">
+									<sup>9</sup> przyimek, łączy się zawsze z Dativ
+								</li>
+								<li className="blue_text">
+									<sup>10</sup> gehen ≈ spazieren
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
