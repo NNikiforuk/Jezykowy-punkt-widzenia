ef734bb9178e36e30a620d85ee6d13375ce3e60a
Natalia <natnikiforuk@gmail.com>
2023-09-12 22:38:52 +0200

    Types for offer/certificate + styling

--- app/oferta/certyfikat/page.tsx
+++ app/oferta/certyfikat/page.tsx
@@ -1,33 +1,13 @@
-import { text } from "../../../data/oferta_certificate";
+import { offer } from "@/data/offer/offer";
 
 const Certificate = () => {
 	return (
 		<div className="certificate">
-			<h1>Przygotowanie do certyfikatów</h1>
+			<h1>{offer[3].name}</h1>
 			<div className="certificate_offer">
-				<p>
-					W nauce języka obcego ważne jest stawianie sobie konkretnych celów.
-					Jednym z nich może być zdobycie certyfikatu, dzięki któremu możemy
-					między innymi ubiegać się o wyższe stanowisko. Potwierdza on znajomość
-					języka niemieckiego na konkretnym poziomie, co jest bardzo przydatne
-					na studiach (szczególnie za granicą) oraz cenione przez pracodawców.
-				</p>
-
-				<p>
-					Goethe Institut to instytucja, która promuje język niemiecki oraz
-					niemiecką kulturę. Jej placówki znaleźć można na całym świecie – w
-					Polsce obecne są w Warszawie i Krakowie. Goethe Institut umożliwia
-					uzyskanie certyfikatu z niemieckiego na każdym poziomie. Certyfikat
-					Goethe jest ważny bezterminowo. Certyfikat z niemieckiego TELC
-					honorują bardziej pracodawcy (w tym urzędy czy służby cywilne) niż
-					uczelnie wyższe. Jego posiadanie wymagane jest też od osób
-					ubiegających się o niemieckie obywatelstwo. Teoretycznie można się przygotować do egzaminu Goethe
-					Zertifikat/ Telc B1 w trakcie 96 godzin intensywnej nauki. Jest to
-					osiągalne, jeżeli uczysz się z lektorem 3h tygodniowo przez
-					8 miesięcy. Oczywiście, to wszystko zależy jednak od danego poziomu
-					osoby uczącej się, dlatego proponujemy indywidualny dobór pakietów
-					godzinowych.
-				</p>
+				<p className="cert bold">{offer[3].desc.text1}</p>
+				<p className="cert">{offer[3].desc.text2}</p>
+				<p className="cert bold">{offer[3].desc.text3}</p>
 			</div>
 		</div>
 	);
--- data/offer/offer.ts
+++ data/offer/offer.ts
@@ -105,6 +105,17 @@ export const offer: OfferProps[] = [
 				"Skontaktuj się z nami, wypełnij test językowy, a my zaproponujemy liczbę godzin oraz kurs.",
 		},
 	},
+	{
+		name: "Przygotowanie do certyfikatów",
+		desc: {
+			text1:
+				"W nauce języka obcego ważne jest stawianie sobie konkretnych celów. Jednym z nich może być zdobycie certyfikatu, dzięki któremu możemy między innymi ubiegać się o wyższe stanowisko. Potwierdza on znajomość języka niemieckiego na konkretnym poziomie, co jest bardzo przydatne na studiach (szczególnie za granicą) oraz cenione przez pracodawców.",
+			text2:
+				"Goethe Institut to instytucja, która promuje język niemiecki oraz niemiecką kulturę. Jej placówki znaleźć można na całym świecie – w Polsce obecne są w Warszawie i Krakowie. Goethe Institut umożliwia uzyskanie certyfikatu z niemieckiego na każdym poziomie.  Certyfikat Goethe jest ważny bezterminowo i honorowany bardziej na uczelniach wyższych, niż przez pracodawców. Certyfikat z niemieckiego TELC honorują bardziej pracodawcy (w tym urzędy czy służby cywilne) niż uczelnie wyższe. Jego posiadanie wymagane jest też od osób ubiegających się o niemieckie obywatelstwo. Certyfikat jest również bezterminowy. Teoretycznie można się przygotować do egzaminu Goethe Zertifikat/ Telc B1 w trakcie 96 godzin intensywnej nauki. Jest to osiągalne , jeżeli uczysz się z lektorem trzy godziny tygodniowo przez 8 miesięcy. Oczywiście to wszystko zależy jednak od danego poziomu osoby uczącej się, dlatego proponujemy indywidualny dobór pakietów godzinowych.",
+			text3:
+				"Napisz do nas, a wyślemy Ci nasz test językowy. Po jego wypełnieniu przedstawimy Ci naszą ofertę i razem dobierzemy pakiety godzinowe.",
+		},
+	},
 ];
 
 interface PackagesProps {
