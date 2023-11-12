a8564bc75cb82cc86e393997d411a4443cf7e06b
Natalia <natnikiforuk@gmail.com>
2023-11-12 22:17:02 +0100

    Children prices

--- app/cennik/page.tsx
+++ app/cennik/page.tsx
@@ -3,6 +3,7 @@ import { links } from "@/data/links";
 import Option from "@/components/prices/Option";
 import womanIMG from "../../public/assets/images/prices/woman.png";
 import manIMG from "../../public/assets/images/prices/man.png";
+import kidIMG from "../../public/assets/images/prices/kid.png";
 
 const Prices = () => {
 	return (
@@ -18,6 +19,11 @@ const Prices = () => {
 					max={4}
 					standardPrices={true}
 					hours92130={true}
+					highlight={false}
+					company={false}
+					lectorOnly={false}
+					individual={false}
+					children={false}
 				/>
 
 				<Option
@@ -29,6 +35,59 @@ const Prices = () => {
 					max={4}
 					standardPrices={true}
 					hours92130={true}
+					highlight={true}
+					company={false}
+					lectorOnly={false}
+					individual={false}
+					children={false}
+				/>
+
+				<Option
+					title="Indywidualny"
+					src={kidIMG}
+					alt="kid"
+					length="Ustalane indywidualnie"
+					lessons="Ustalane indywidualnie spotkania 60 min"
+					max={1}
+					standardPrices={false}
+					hours92130={false}
+					highlight={false}
+					company={false}
+					lectorOnly={false}
+					individual={false}
+					children={false}
+				/>
+
+				<Option
+					title="Kurs branżowy"
+					src={womanIMG}
+					alt="woman"
+					length="3/4 miesięce"
+					lessons="24 (36h) spotkania x 1,5 h / 2x w tygodniu"
+					max={6}
+					standardPrices={false}
+					hours92130={true}
+					highlight={false}
+					company={true}
+					lectorOnly={true}
+					individual={false}
+					children={false}
+				/>
+
+				<Option
+					title="Dzieci i młodzież"
+					src={kidIMG}
+					alt="kid"
+					length="3/4 miesięce, kursy wakacyjne"
+					lessons="24 (36h) spotkania x 1,5 h / 2x w tygodniu"
+					max={4}
+					standardPrices={false}
+					hours92130={true}
+					highlight={false}
+					company={false}
+					lectorOnly={false}
+					individual={false}
+					children={true}
 				/>
 			</div>
 		</main>
--- components/prices/Option.tsx
+++ components/prices/Option.tsx
@@ -11,19 +11,12 @@ interface Props {
 	max: number;
 	standardPrices: boolean;
 	hours92130: boolean;
+	highlight: boolean;
+	company: boolean;
+	lectorOnly: boolean;
+	individual: boolean;
+	children: boolean;
 }
-
-const Hours92130 = () => {
-	return (
-		<div>
-			<p className="desc">9.00 – 10.30</p>
-			<p className="desc">11.00 – 12.30</p>
-			<p className="desc">13.00 – 14.30</p>
-			<p className="desc">18.00 – 19.30</p>
-			<p className="desc">20.00 – 21.30</p>
-		</div>
-	);
-};
 
 const Standard_prices = () => {
 	return (
@@ -31,6 +24,28 @@ const Standard_prices = () => {
 			<p>2-osobowa: 2 340</p>
 			<p>3-osobowa: 1 584</p>
 			<p>4-osobowa: 1 188</p>
+		</div>
+	);
+};
+
+const Average_prices = () => {
+	return (
+		<div className="desc">
+			<p>2-osobowa: 3 840</p>
+			<p>3-osobowa: 1 944</p>
+			<p>4-osobowa: 1 440</p>
+			<p>5-osobowa: 1 152</p>
+			<p>6-osobowa: 984</p>
+		</div>
+	);
+};
+
+const Children_prices = () => {
+	return (
+		<div className="desc">
+			<p>2-osobowa: 1 800</p>
+			<p>3-osobowa: 1 224</p>
+			<p>4-osobowa: 900</p>
 		</div>
 	);
 };
@@ -44,9 +59,26 @@ const Option = ({
 	max,
 	standardPrices,
 	hours92130,
+	highlight,
+	company,
+	lectorOnly,
+	individual,
+	children,
 }: Props) => {
+	const Hours92130 = () => {
+		return (
+			<div>
+				<p className="desc">9.00 – 10.30</p>
+				<p className="desc">11.00 – 12.30</p>
+				<p className="desc">13.00 – 14.30</p>
+				<p className="desc">18.00 – 19.30</p>
+				{children ? null : <p className="desc">20.00 – 21.30</p>}
+			</div>
+		);
+	};
+
 	return (
-		<div className="container">
+		<div className={`container ${highlight && "highlight"}`}>
 			<h2>{title}</h2>
 			<div className="option_img">
 				<Image src={src} alt={alt} width={128} className="img" />
@@ -59,11 +91,20 @@ const Option = ({
 					</div>
 					<div className="row">
 						<p className="title">POZIOM</p>
-						<p className="desc">A1 - C2</p>
+
+						{company ? (
+							<p className="desc">B2 - C2</p>
+						) : (
+							<p className="desc">A1 - C2</p>
+						)}
 					</div>
 					<div className="row">
 						<p className="title">NAUCZYCIEL</p>
-						<p className="desc">Native speaker lub wykwalifikowany lektor</p>
+						{lectorOnly ? (
+							<p className="desc">Wykwalifikowany lektor</p>
+						) : (
+							<p className="desc">Native speaker lub wykwalifikowany lektor</p>
+						)}
 					</div>
 					<div className="row">
 						<p className="title">LICZBA LEKCJI W PAKIECIE</p>
@@ -75,7 +116,11 @@ const Option = ({
 					</div>
 					<div className="row">
 						<p className="title">GODZINY ZAJĘĆ</p>
-						{hours92130 ? <Hours92130 /> : null}
+						{hours92130 ? (
+							<Hours92130 />
+						) : (
+							<p className="desc">"Ustalane indywidualnie"</p>
+						)}
 					</div>
 					<div className="row">
 						<p className="title">TRYB ZAJĘĆ</p>
@@ -95,7 +140,15 @@ const Option = ({
 					</div>
 					<div className="row total">
 						<p className="title">CENA ZA OSOBĘ W GRUPIE [ZŁ]</p>
-						{standardPrices ? <Standard_prices /> : null}
+						{standardPrices ? (
+							<Standard_prices />
+						) : company ? (
+							<Average_prices />
+						) : children ? (
+							<Children_prices />
+						) : (
+							<div className="desc">130 zł / 60 min</div>
+						)}
 					</div>
 				</div>
 			</div>
