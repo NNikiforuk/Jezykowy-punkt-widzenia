ef34e48fe3421bbfbfe0e76eb2ddb70282536e35
Natalia <natnikiforuk@gmail.com>
2023-11-20 17:06:45 +0100

    New texts in translations

--- app/tlumaczenia/page.tsx
+++ app/tlumaczenia/page.tsx
@@ -1,11 +1,10 @@
 import { FaMedal, FaCheck } from "react-icons/fa";
-import { translations } from "@/data/translations/translations";
 import Image from "next/image";
 import { links } from "@/data/links";
 
 const Translations = () => {
 	return (
-		<div className="translations">
+		<main className="translations">
 			<div className="translations_img">
 				<Image
 					src={links[2].img}
@@ -18,32 +17,77 @@ const Translations = () => {
 				/>
 			</div>
 			<div className="translations_container">
-				{translations.list1.map((l) => (
-					<div className="translations_desc" key={l}>
+				<div className="list">
+					<div className="translations_desc">
 						<div className="translations_icon">
 							<FaMedal />
 						</div>
-						<div className="translations_text">{l}</div>
+						<div className="translations_text">
+							Wykonujemy tłumaczenia pisemne zwykłe, nieopatrzone pieczęcią
+						</div>
 					</div>
-				))}
-				<div className="translations_list">
-					<div className="translations_list_title">
-						{translations.list2.title}
+					<div className="translations_desc">
+						<div className="translations_icon">
+							<FaMedal />
+						</div>
+
+						<div className="translations_text">
+							Gwarantujemy rzetelne, dokładne i obiektywne tłumaczenia z
+							zasadami zachowania wierności tłumaczonego tekstu
+						</div>
 					</div>
-					<ul className="translations_list">
-						{translations.list2.list.map((l) => (
-							<li key={l}>
-								<div className="translations_item_icon">
-									<FaCheck />
-								</div>
-								<div className="translations_item_text">{l}</div>
-							</li>
-						))}
-					</ul>
 				</div>
-				<div className="ps">{translations.text1}</div>
+
+				<div className="translations_list">
+					<div className="translations_list_title">Oferujemy tłumaczenie:</div>
+					<div className="list">
+						<div className="list_item">
+							<div className="translations_item_icon">
+								<FaCheck />
+							</div>
+							<div className="translations_item_text">listów, artykułów</div>
+						</div>
+						<div className="list_item">
+							<div className="translations_item_icon">
+								<FaCheck />
+							</div>
+							<div className="translations_item_text">
+								instrukcji obsługi urządzeń lub sprzętu
+							</div>
+						</div>
+						<div className="list_item">
+							<div className="translations_item_icon">
+								<FaCheck />
+							</div>
+							<div className="translations_item_text">
+								broszur, ulotek, katalogów
+							</div>
+						</div>
+						<div className="list_item">
+							<div className="translations_item_icon">
+								<FaCheck />
+							</div>
+							<div className="translations_item_text">stron internetowych</div>
+						</div>
+						<div className="list_item">
+							<div className="translations_item_icon">
+								<FaCheck />
+							</div>
+							<div className="translations_item_text">publikacji naukowych</div>
+						</div>
+						<div className="list_item">
+							<div className="translations_item_icon">
+								<FaCheck />
+							</div>
+							<div className="translations_item_text">
+								opisów produktów, tekstów biznesowych i marketingowych
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="ps">Skontaktuj się w celu uzgodnienia szczegółów!</div>
 			</div>
-		</div>
+		</main>
 	);
 };
 
