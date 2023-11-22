4e0cd977a6f875659ca515ac5e9b7652fd132992
Natalia <natnikiforuk@gmail.com>
2023-11-22 22:11:17 +0100

    Ribbon on landing page for mobiles

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
-import { CgArrowLongRightR } from "react-icons/cg";
+import { IoTriangleOutline } from "react-icons/io5";
 import { RiArrowLeftSFill, RiArrowRightSFill } from "react-icons/ri";
+
+import { MdOutlineCompareArrows } from "react-icons/md";
+
 import Image from "next/legacy/image";
 import img from "../public/assets/images/landing_page/home.jpg";
 import Box from "@/components/home/Box";
@@ -10,7 +13,7 @@ const Home = () => {
 			<div className="home_mobile">
 				<h1 className="home_headline">JĘZYKOWY PUNKT WIDZENIA</h1>
 				<div className="arrow_icon">
-					<CgArrowLongRightR />
+					<MdOutlineCompareArrows />
 				</div>
 				<h2 className="home_headline-secondary">KURSY JĘZYKOWE</h2>
 				<p>ZAJĘCIA ONLINE</p>
@@ -23,6 +26,10 @@ const Home = () => {
 						<RiArrowRightSFill />
 					</div>
 				</div>
+			</div>
+			<div className="css_element">
+				<div className="element left"></div>
+				<div className="element right"></div>
 			</div>
 			<div className="home_boxes_mobile">
 				<Box />
