1683b4d72c8f29f0d221cf6e76ea193be641140d
Natalia <natnikiforuk@gmail.com>
2023-11-27 23:31:27 +0100

    Gooter with just text + styling

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -3,7 +3,43 @@ const Footer = () => {
 
 	return (
 		<footer className="footer">
-			&copy; {`${year} Językowy punkt widzenia`}
+			<div className="footer_content">
+				<div className="info">
+					<div className="title">Informacje</div>
+					<div className="privacy_policy">
+						<div>Polityka prywatności</div>
+					</div>
+					<div className="statute">
+						<div>Regulamin kursu online</div>
+					</div>
+					<div className="data">
+						<div>Przetwarzanie danych osobowych</div>
+					</div>
+				</div>
+				<div className="second_column">
+					<div className="company_data">
+						<div className="title">Dane firmy</div>
+						<>
+							<div>JĘZYKOWY PUNKT WIDZENIA</div>
+							<div>z siedzibą w Międzylesiu 17</div>
+							<div>67 – 312 Niegosławice</div>
+						</>
+					</div>
+					<div className="sub_column">
+						<div className="nip_regon_contact">
+							<div>REGON: 386531965</div>
+							<div>NIP: 9241915728</div>
+						</div>
+						<div className="footer_contact">
+							<div>tel.: (+48) 603 156 142 </div>
+							<div>e-mail: kddobrzanska@gmail.com </div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="footer_year">
+				&copy; {`${year} Językowy punkt widzenia`}
+			</div>
 		</footer>
 	);
 };
