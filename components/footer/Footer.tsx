2b782e05fb0bfe184fc5a303ab5a7cbbf8548622
Natalia <natnikiforuk@gmail.com>
2023-11-28 19:42:25 +0100

    Social icons / footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { FaFacebookF, FaInstagram, FaTiktok, FaYoutube } from "react-icons/fa6";
 
 const Footer = () => {
 	const year = new Date().getFullYear();
@@ -6,46 +7,78 @@ const Footer = () => {
 	return (
 		<footer className="footer">
 			<div className="footer_content">
-				<div className="info">
-					<div className="title">Informacje</div>
-					<div className="privacy_policy">
-						<Link href="/assets/files/Polityka_prywatnosci.pdf">
-							<div>Polityka prywatności</div>
-						</Link>
-					</div>
-					<div className="statute">
-						<Link href="/assets/files/Regulamin.pdf">
-							<div>Regulamin kursu online</div>
-						</Link>
-					</div>
-					<div className="data">
-						<Link href="/assets/files/Przetwarzanie_danych_osobowych.pdf">
-							<div>Przetwarzanie danych osobowych</div>
-						</Link>
-					</div>
+				<div className="icons">
+					<Link
+						target="_blank"
+						className="icon"
+						href="https://www.facebook.com/profile.php?id=100077329971505"
+					>
+						<FaFacebookF />
+					</Link>
+					<Link
+						target="_blank"
+						className="icon"
+						href="https://www.youtube.com/@jezykowypunktwidzenia"
+					>
+						<FaYoutube />
+					</Link>
+					<Link
+						target="_blank"
+						className="icon"
+						href="https://www.instagram.com/jpw_jezykowy_punkt_widzenia/"
+					>
+						<FaInstagram />
+					</Link>
+					<Link
+						target="_blank"
+						className="icon"
+						href="https://www.tiktok.com/@kasiaddobrzanska"
+					>
+						<FaTiktok />
+					</Link>
 				</div>
-				<div className="second_column">
-					<div className="company_data">
-						<div className="title">Dane firmy</div>
-						<>
-							<div>JĘZYKOWY PUNKT WIDZENIA</div>
-							<div>z siedzibą w Międzylesiu 17</div>
-							<div>67 – 312 Niegosławice</div>
-						</>
-					</div>
-					<div className="sub_column">
-						<div className="nip_regon_contact">
-							<div>REGON: 386531965</div>
-							<div>NIP: 9241915728</div>
+				<div className="text_container">
+					<div className="info">
+						<div className="title">Informacje</div>
+						<div className="privacy_policy">
+							<Link href="/assets/files/Polityka_prywatnosci.pdf">
+								<div>Polityka prywatności</div>
+							</Link>
 						</div>
-						<div className="footer_contact">
-							<div>
-								<Link href="tel:48603156142">(+48) 603 156 142</Link>
+						<div className="statute">
+							<Link href="/assets/files/Regulamin.pdf">
+								<div>Regulamin kursu online</div>
+							</Link>
+						</div>
+						<div className="data">
+							<Link href="/assets/files/Przetwarzanie_danych_osobowych.pdf">
+								<div>Przetwarzanie danych osobowych</div>
+							</Link>
+						</div>
+					</div>
+					<div className="second_column">
+						<div className="company_data">
+							<div className="title">Dane firmy</div>
+							<>
+								<div>JĘZYKOWY PUNKT WIDZENIA</div>
+								<div>z siedzibą w Międzylesiu 17</div>
+								<div>67 – 312 Niegosławice</div>
+							</>
+						</div>
+						<div className="sub_column">
+							<div className="nip_regon_contact">
+								<div>REGON: 386531965</div>
+								<div>NIP: 9241915728</div>
 							</div>
-							<div className="email_address">
-								<Link href="mailto:kddobrzanska@gmail.com">
-									kddobrzanska@gmail.com
-								</Link>
+							<div className="footer_contact">
+								<div>
+									<Link href="tel:48603156142">(+48) 603 156 142</Link>
+								</div>
+								<div className="email_address">
+									<Link href="mailto:kddobrzanska@gmail.com">
+										kddobrzanska@gmail.com
+									</Link>
+								</div>
 							</div>
 						</div>
 					</div>
