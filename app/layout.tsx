872773be8b82aa4c73ac92fb94ad8d2bc1bb4b75
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-01-10 15:48:29 +0100

    Fix: meta tags not shoeing on google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import "./globals.scss";
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import Footer from "@/components/footer/Footer";
 import Nav from "../components/navigation/Navigation";
+import Head from "next/head";
 
 const openSans = Open_Sans({ subsets: ["latin"], weight: ["400", "700"] });
 
-export const metadata: Metadata = {
-	title: "Językowy punkt widzenia",
-	description:
-		"Z nami nauka języków jest przyjemna, a także przynosi rezultaty",
-};
+// export const metadata: Metadata = {
+// 	title: "Językowy punkt widzenia",
+// 	description:
+// 		"Z nami nauka języków jest przyjemna, a także przynosi rezultaty",
+// };
 
 export default function RootLayout({
 	children,
@@ -19,6 +20,13 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="pl">
+			<Head>
+				<title>Językowy punkt widzenia</title>
+				<meta
+					name="description"
+					content="Z nami nauka języków jest przyjemna, a także przynosi rezultaty"
+				/>
+			</Head>
 			<body className={openSans.className}>
 				<Nav />
 				{children}
